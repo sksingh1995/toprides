3659cf8885cdc035788ae55aa1da98c89159fbb1
Shobhit Singh <shobhitsingh650@gmail.com>
2019-02-19 23:16:38 +0530

    document upload for  enrollemnt

--- src/app/modules/enrollment/new-enrollment/new-enrollment.component.ts
+++ src/app/modules/enrollment/new-enrollment/new-enrollment.component.ts
@@ -1,6 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { HttpService } from "../../../services/http.service";
-import { NgForm } from "@angular/forms";
+import { NgForm, NgModel } from "@angular/forms";
 
 @Component({
   selector: "toprides-new-enrollment",
@@ -8,13 +8,35 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./new-enrollment.component.css"]
 })
 export class NewEnrollmentComponent {
-  private postURL = "http://52.66.235.25/toprides-server/post.php";
+  private postURL = "http://52.66.235.25/toprides-server/sendmail.php";
   public submitting: Boolean = false;
   public enrolled: Boolean = false;
+  public files: Array<any> = [];
+  public avatar: string = "";
+  public uploadedFiles: any = "";
 
   constructor(private http: HttpService) {}
 
+  handleFileChange(e) {
+    this.files = e.target.files;
+    this.uploadedFiles = this.files.length;
+  }
+
+  handleAvatarUpload(e) {
+    let reader = new FileReader();
+
+    reader.onload = e => {
+      this.avatar = e.target.result;
+    };
+
+    reader.readAsDataURL(e.target.files[0]);
+  }
+
   onNewRegistration(form: NgForm) {
+    if (this.avatar == "" || this.files.length == 0) {
+      return false;
+    }
+
     this.submitting = true;
     let formdata = new FormData();
 
@@ -24,13 +46,18 @@ export class NewEnrollmentComponent {
       formdata.append(el, values[el]);
     });
 
+    for (let i = 0; i < this.files.length; i++) {
+      formdata.append("attachments[]", this.files[i], this.files[i].name);
+    }
+
     this.http
       .post(this.postURL, formdata)
       .then(res => {
         this.submitting = false;
         form.resetForm();
         this.enrolled = true;
-
+        this.avatar = "";
+        this.files = [];
         setTimeout(() => {
           this.enrolled = false;
         }, 3000);
