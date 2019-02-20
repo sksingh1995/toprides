0c99f89b26e46146d50526f38432ec45c02de4a0
Shobhit Singh <shobhitsingh650@gmail.com>
2019-02-21 00:25:12 +0530

    added 3 enrollment forms for driver cab and owner

--- src/app/modules/enrollment/enrollment.module.ts
+++ src/app/modules/enrollment/enrollment.module.ts
@@ -4,14 +4,23 @@ import { RouterModule, Routes } from "@angular/router";
 import { EnrollmentComponent } from "./enrollment.component";
 import { NewEnrollmentComponent } from "./new-enrollment/new-enrollment.component";
 import { SharedModule } from "../shared.module";
+import { DriverEnrollmentComponent } from "./driver-enrollment/driver-enrollment.component";
+import { CabEnrollmentComponent } from "./cab-enrollment/cab-enrollment.component";
 
 const routes: Routes = [
   { path: "", component: EnrollmentComponent },
-  { path: "new", component: NewEnrollmentComponent }
+  { path: "new", component: NewEnrollmentComponent },
+  { path: "driver", component: DriverEnrollmentComponent },
+  { path: "cab", component: CabEnrollmentComponent }
 ];
 
 @NgModule({
-  declarations: [EnrollmentComponent, NewEnrollmentComponent],
+  declarations: [
+    EnrollmentComponent,
+    NewEnrollmentComponent,
+    DriverEnrollmentComponent,
+    CabEnrollmentComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(routes), SharedModule]
 })
 export class EnrollmentModule {}
