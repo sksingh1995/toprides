1a481470379bb96e22dc05fefe2e345b36040e7c
Shobhit Singh <shobhitsingh650@gmail.com>
2019-02-23 18:00:46 +0530

    sending email from contact us form

--- src/app/modules/contactus/contactus.module.ts
+++ src/app/modules/contactus/contactus.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
 import { ContactUsComponent } from "./contactus.component";
+import { SharedModule } from "../shared.module";
 
 const routes: Routes = [{ path: "", component: ContactUsComponent }];
 
 @NgModule({
   declarations: [ContactUsComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)]
+  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)]
 })
 export class ContactUsModule {}
